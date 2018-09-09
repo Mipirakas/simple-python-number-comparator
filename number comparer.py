74bd0d4e12ede08fa577378e1185ba84facd0b0d
Boris Michiels <borismichiels@gmail.com>
2018-09-10 01:41:51 +0200

    Initial commit

---
+++ number comparer.py
@@ -0,0 +1,8 @@
+print("Welcome to this state of the art ;) number comparer. Give me 2 numbers and i'll compare them for you.")
+print("Input your first number below:\n")
+N1 = input("First Number: ")
+N2 = input("Second Number: ")
+if N1 == N2:
+	print("\nNumber 1 and 2 are equal")
+else:
+	print("\nThe 2 numbers are not equal")
