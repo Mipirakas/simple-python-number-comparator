36bddcec01574f8fbfa9e246fca7e52a45ebf900
Boris Michiels <borismichiels@gmail.com>
2018-09-10 04:49:54 +0200

    Now an actual hash comparer: v2.0 Is able to compare strings now instead of only numbers.

--- number comparer.py
+++
@@ -1,9 +0,0 @@
-print("Welcome to this state of the art ;) number comparer.\nInput your 2 numbers below and i'll compare them for you.\n")
-
-N1 = input("Number 1: ")
-N2 = input("Number 2: ")
-
-if N1 == N2:
-	print("\nNumber 1 and 2 are equal")
-else:
-	print("\nThe 2 numbers are not equal")
---
+++ string comparer.py
@@ -0,0 +1,11 @@
+print("Simple string comparer v2.0 by Mipirakas\n\nWelcome to this state of the art ;) string comparer.\nInput your 2 strings below and i'll compare them for you.\n")
+
+N1 = raw_input("String 1: ")
+N2 = raw_input("String 2: ")
+
+if N1 == N2:
+	print("\nString 1 and 2 are equal")
+else:
+	print("\nThe 2 Strings are different")
+
+#print("\nSimple string comparer v2.0 by Mipirakas")
