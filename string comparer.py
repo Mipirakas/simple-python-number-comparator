84bdd9c2dd980b4d560c0279155e8e525d0e6bf3
Boris Michiels <borismichiels@gmail.com>
2018-09-10 04:57:31 +0200

    New variable name to reflect v2.0 changes

--- string comparer.py
+++ string comparer.py
@@ -1,9 +1,9 @@
 print("Simple string comparer v2.0 by Mipirakas\n\nWelcome to this state of the art ;) string comparer.\nInput your 2 strings below and i'll compare them for you.\n")
 
-N1 = raw_input("String 1: ")
-N2 = raw_input("String 2: ")
+str1 = raw_input("String 1: ")
+str2 = raw_input("String 2: ")
 
-if N1 == N2:
+if str1 == str2:
 	print("\nString 1 and 2 are equal")
 else:
 	print("\nThe 2 Strings are different")
