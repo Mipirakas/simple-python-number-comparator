04f6306f7a4e05c3a6fc696cd5c70887b0874f32
Boris Michiels <borismichiels@gmail.com>
2018-09-11 17:12:51 +0200

    add alphabetic ranking and number ranking for positive numbers

--- string comparer.py
+++ string comparer.py
@@ -3,7 +3,15 @@ print("Simple string comparer v2.0 by Mipirakas\n\nWelcome to this state of the 
 str1 = raw_input("String 1: ")
 str2 = raw_input("String 2: ")
 
+print("\n")
+
 if str1 == str2:
-	print("\nString 1 and 2 are equal")
+	print("String 1 and 2 are equal.")
 else:
-	print("\nThe 2 Strings are different")
+	print("The two strings are different and are arranged alphabetically and from smallest number to biggest number. (does not work for negative numbers)\n")
+
+	if str1 > str2:
+		print("String 2) " + str2 + "\nString 1) " + str1)
+
+	if str1 < str2:
+		print("String 1) " + str1 + "\nString 2) " + str2)
